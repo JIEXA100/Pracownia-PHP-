ad1a999583220648af584045fb254cd0af2520d2
JIEXA100 <90255121+JIEXA100@users.noreply.github.com>
2021-09-20 22:28:10 +0200

    Delete logowanie directory

--- logowanie/zadanie23c.php
+++
@@ -1,7 +0,0 @@
-<form action="zakladanie_kapshuk.php" method="post">
-<input name="logowanie" type="submit" value="Przejdź do rejestracji">
-</form>
-
-<form action="logowanie_kapshuk.php" method="post">
-<input name="logowanie" type="submit" value="Przejdź do logowania">
-</form>
