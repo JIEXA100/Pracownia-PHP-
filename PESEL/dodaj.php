2574bb567e15b3a85b5c325eda05eef46e59b6d0
JIEXA100 <90255121+JIEXA100@users.noreply.github.com>
2021-10-20 09:59:03 +0200

    Add files via upload

---
+++ PESEL/dodaj.php
@@ -0,0 +1,22 @@
+<?php
+    $user= 'root';
+    $pass= '';
+    $host = 'localhost';
+    $base = 'dane'; 
+    $con= mysqli_connect($host,$user,$pass, $base);
+    mysqli_select_db($con,$base);
+
+    if(isset($_POST['sub']))
+		 {
+             $Tytul = $_POST['titl'];
+             $Gatunek = $_POST['gat'];
+             $Rok = $_POST['prod'];
+             $Ocena = $_POST['mark'];
+             
+   $query = "insert into filmy (gatunki_id,tytul,rok,ocena) VALUES ($Gatunek, '$Tytul',$Rok,$Ocena)";
+
+   $run =mysqli_query($con,$query) or die(mysqli_error($con));
+   
+   echo "Film " . $Tytul . " został dodany do bazy";
+         }
+?>
